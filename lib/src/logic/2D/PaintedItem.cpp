4bbdf0a8d0b22e44c94d0017c106b13b7446bd36
Remi Gastaldi <remi.gastaldi@epitech.eu>
2019-03-06 20:45:42 -0500

    Protect event if scene2D is null

--- lib/src/logic/2D/PaintedItem.cpp
+++ lib/src/logic/2D/PaintedItem.cpp
@@ -6,24 +6,29 @@
 namespace ART {
 namespace Logic {
 
+void PaintedItem::setScene2D(Scene2D *scene2D) {
+  _scene2D = scene2D;
+}
+
 void PaintedItem::paint(QPainter *painter) {
   if (_scene2D != nullptr) {
     _scene2D->paint(painter);
   }
 }
 
-void PaintedItem::setScene2D(Scene2D *scene2D) {
-  _scene2D = scene2D;
+void PaintedItem::mousePressEvent(QMouseEvent *event) { 
+  if (_scene2D != nullptr)
+    _scene2D->mousePressEvent(event);
 }
 
-void PaintedItem::mousePressEvent(QMouseEvent *event) { 
-  _scene2D->mousePressEvent(event);
-}
 void PaintedItem::mouseMoveEvent(QMouseEvent *event) { 
-  _scene2D->mouseMoveEvent(event);
+  if (_scene2D != nullptr)
+    _scene2D->mouseMoveEvent(event);
 }
+
 void PaintedItem::mouseReleaseEvent(QMouseEvent *event) { 
-  _scene2D->mouseReleaseEvent(event);
+  if (_scene2D != nullptr)
+    _scene2D->mouseReleaseEvent(event);
 }
 
 } // namespace Logic
