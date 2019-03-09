1e2f1a56c462a67949a6891104c960b25fe7a9e4
Remi Gastaldi <remi.gastaldi@epitech.eu>
2019-03-09 18:28:03 -0500

    [+] Transform c++ file

--- lib/public/logic/3D/modules/Transform3D.hpp
+++ lib/public/logic/3D/modules/Transform3D.hpp
@@ -17,56 +17,22 @@ class Transform3D : public Entity {
   Q_PROPERTY(float z READ z NOTIFY dataUpdate)
 
 public:
-  Transform3D(Shape3D &parent, std::string id,
-            Qt3DCore::QTransform *transform = new Qt3DCore::QTransform)
-      : Entity{std::move(id)}, _transform{transform}, _parent{parent} {
-    _transform->setParent(parent.getQEntity());
-    parent.getQEntity()->addComponent(_transform);
-    parent.addChildren(Entity::id(), std::unique_ptr<Entity>(this));
-  }
+  Transform3D(Shape3D &parent, std::string id, Qt3DCore::QTransform *transform = new Qt3DCore::QTransform);
 
-  virtual ~Transform3D() {
-    _parent.getQEntity()->removeComponent(_transform);
-    delete _transform;
-  }
+      virtual ~Transform3D();
 
-  float x() {
-    return _transform->translation().x();
-  }
+  float x();
+  float y();
+  float z();
 
-  float y() {
-    return _transform->translation().x();
-  }
+  Qt3DCore::QTransform *get() noexcept;
 
-  float z() {
-    return _transform->translation().x();
-  }
-
-  Qt3DCore::QTransform *get() noexcept { return _transform; }
-  
-  Qt3DCore::QTransform *operator->() {
-    return _transform;
-  };
+  Qt3DCore::QTransform *operator->() { return _transform; };
 
 public Q_SLOTS:
-  void setX(double x) noexcept {
-    QVector3D trans{_transform->translation()};
-    trans.setX(x);
-    qInfo() << trans;
-    _transform->setTranslation(std::move(trans));
-  }
-
-  void setY(double y) noexcept {
-    QVector3D trans{_transform->translation()};
-    trans.setY(y);
-    _transform->setTranslation(std::move(trans));
-  }
-
-  void setZ(double y) noexcept {
-    QVector3D trans{_transform->translation()};
-    trans.setZ(y);
-    _transform->setTranslation(std::move(trans));
-  }
+  void setX(double x) noexcept;
+  void setY(double y) noexcept;
+  void setZ(double y) noexcept;
 
 Q_SIGNALS:
   void dataUpdate();
@@ -77,7 +43,7 @@ private:
 };
 
 // Qt3DCore::QTransform * Transform::operator->() {
-  // return _transform;
+// return _transform;
 // }
 
 } // namespace Modules
---
+++ lib/src/logic/3D/modules/Transform3D.cpp
@@ -0,0 +1,48 @@
+#include "Transform3D.hpp"
+
+namespace ART {
+namespace Logic {
+namespace Modules {
+
+Transform3D::Transform3D(Shape3D &parent, std::string id, Qt3DCore::QTransform *transform)
+    : Entity{std::move(id)}, _transform{transform}, _parent{parent} {
+  _transform->setParent(parent.getQEntity());
+  parent.getQEntity()->addComponent(_transform);
+  parent.addChildren(Entity::id(), std::unique_ptr<Entity>(this));
+}
+
+Transform3D::~Transform3D() {
+  _parent.getQEntity()->removeComponent(_transform);
+  delete _transform;
+}
+
+float Transform3D::x() { return _transform->translation().x(); }
+
+float Transform3D::y() { return _transform->translation().x(); }
+
+float Transform3D::z() { return _transform->translation().x(); }
+
+Qt3DCore::QTransform *Transform3D::get() noexcept { return _transform; }
+
+  void Transform3D::setX(double x) noexcept {
+  QVector3D trans{_transform->translation()};
+  trans.setX(x);
+  qInfo() << trans;
+  _transform->setTranslation(std::move(trans));
+}
+
+void Transform3D::setY(double y) noexcept {
+  QVector3D trans{_transform->translation()};
+  trans.setY(y);
+  _transform->setTranslation(std::move(trans));
+}
+
+void Transform3D::setZ(double y) noexcept {
+  QVector3D trans{_transform->translation()};
+  trans.setZ(y);
+  _transform->setTranslation(std::move(trans));
+}
+
+} // namespace Modules
+} // namespace Logic
+} // namespace ART
