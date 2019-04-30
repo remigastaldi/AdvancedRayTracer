4b5578d9a6b705cf2e573d3535941c7e46bbe25c
Léo <leohub@live.fr>
2019-04-30 03:36:12 -0400

    Add ConeMesh Clean Code

--- lib/public/logic/3D/modules/lights/SpotLight.hpp
+++ lib/public/logic/3D/modules/lights/SpotLight.hpp
@@ -3,17 +3,33 @@
 #include "Light.hpp"
 #include "Shape3D.hpp"
 
+#include "ConeMesh.hpp"
+#include "PhongAlphaMaterial.hpp"
+
+#include <Qt3DCore>
+
 namespace ART::Logic::Modules::Lights {
 
 class SpotLight : public Light<Qt3DRender::QSpotLight> {
-
+  Q_OBJECT
+  Q_PROPERTY(QColor color READ color NOTIFY dataUpdate)
+  Q_DISABLE_COPY(SpotLight)
 public:
-  SpotLight(Shape3D &parent, std::string id)
-      : Light<Qt3DRender::QSpotLight>{parent, id} {
-    get()->setColor("green");
-    get()->setIntensity(1);
-    get()->setLocalDirection({0, 0, 1});
-  }
+  SpotLight(Shape3D &parent, std::string id);
+
+  virtual ~SpotLight();
+
+  QColor color();
+
+public Q_SLOTS:
+
+  void setColor(const QColor &color);
+
+Q_SIGNALS:
+  void dataUpdate();
+
+private:
+  PhongAlphaMaterial *_material;
 };
 
 } // namespace ART::Logic::Modules::Lights
